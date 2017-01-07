b5998be5e79197e1b7c8d51b154b203915f6c98c
Chris Bubel <christopher.s.bubel@gmail.com>
2017-01-07 18:52:15 -0500

    Updating router functionality

--- js/router.js
+++ js/router.js
@@ -1,3 +1,10 @@
-function load(filename) {
-    document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${filename}.html"></object>`;
+document.addEventListener("onload", function() {
+    const url = window.location.href;
+    const path = url.split(".com")[1];
+    console.log(`loading ${path}`);
+    load(path);
+});
+
+function load(path) {
+    document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${path}.html"></object>`;
 };
