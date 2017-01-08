e8e9c154eafca0fa904e625c376e79e22d8d9a07
Chris Bubel <christopher.s.bubel@gmail.com>
2017-01-07 19:24:15 -0500

    semantics

--- js/router.js
+++ js/router.js
@@ -1,6 +1,8 @@
+const HOME_PARTIAL = "home"; // home partial filename
+
 function load() {
     let path = getPath();
-    if(path === "" || path === undefined) path = "home";
+    if(path === "" || path === undefined) path = HOME_PARTIAL;
     document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${path}.html"></object>`;
 };
 
