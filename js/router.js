6e41ed670a277e317b497bc9f0b1529037119e39
Chris Bubel <christopher.s.bubel@gmail.com>
2017-01-07 19:01:14 -0500

    prevent default event test

--- js/router.js
+++ js/router.js
@@ -2,7 +2,8 @@ function load(path) {
     document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${path}.html"></object>`;
 };
 
-window.onhashchange = function() {
+window.onhashchange = function(event) {
+    event.preventDefault();
     const url = window.location.href;
     const path = url.split(".com")[1];
     if(path === undefined) return;
