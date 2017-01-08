22cc64f4b2e5fd00791b38c32893d04a73547915
Chris Bubel <christopher.s.bubel@gmail.com>
2017-01-07 19:00:03 -0500

    Updating for on hash change

--- js/router.js
+++ js/router.js
@@ -1,10 +1,11 @@
-document.addEventListener("onload", function() {
-    const url = window.location.href;
-    const path = url.split(".com")[1];
-    console.log(`loading ${path}`);
-    load(path);
-});
-
 function load(path) {
     document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${path}.html"></object>`;
+};
+
+window.onhashchange = function() {
+    const url = window.location.href;
+    const path = url.split(".com")[1];
+    if(path === undefined) return;
+    console.log(`loading ${path}`);
+    load(path);
 };
